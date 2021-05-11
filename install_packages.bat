d12fb44a67eedaf1a11a83fcd8d01ba3e79db3f6
Truong Hoang <10404416+truonghm@users.noreply.github.com>
2021-05-11 20:48:22 +0700

    Add files via upload

---
+++ install_packages.bat
@@ -0,0 +1,12 @@
+
+python -m pip install --upgrade pip --quiet
+
+pip install numpy==1.19.2 --quiet
+pip install tensorflow==2.4.1 --quiet
+pip install ann-visualizer==2.5 --quiet
+pip install matplotlib==3.4.1 --quiet
+pip install pandas==1.2.4 --quiet
+pip install scikit-learn==0.24.2 --quiet
+pip install scipy==1.6.3 --quiet
+pip install seaborn==0.11.1 --quiet
+pip install xarray==0.17.0 --quiet
